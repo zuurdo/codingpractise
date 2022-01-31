4d56a79f587c3d6fc0b3b75944e3b9a777d2f9d8
Raul Martin <raul_martin_pintos@hotmail.com>
2022-01-31 13:40:49 +0000

    Add main code to comparator challenge

--- hackerRankPractise/interviewPreparationKit/sorting/comparator/comparator.cpp
+++ hackerRankPractise/interviewPreparationKit/sorting/comparator/comparator.cpp
@@ -1,3 +1,12 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Player {
+    int score;
+    string name;
+};
 
 class Checker{
 public:
@@ -30,3 +39,30 @@ public:
       }
     }
 };
+
+bool compare(Player a, Player b) {
+  if(Checker::comparator(a,b) == -1)
+    return false;
+  return true;
+}
+int main()
+{
+  int n;
+  cin >> n;
+  vector< Player > players;
+  string name;
+  int score;
+  for(int i = 0; i < n; i++){
+    cin >> name >> score;
+    Player player;
+    player.name = name, player.score = score;
+    players.push_back(player);
+  }
+  sort(players.begin(), players.end(), compare);
+  for(int i = 0; i < players.size(); i++) {
+    cout << players[i].name << " " << players[i].score << endl;
+  }
+  return 0;
+}
+
+
